7340edb26b06ebdb0f6dc9e04b155f8d9c8d3566
Caralyn Reisle <creisle@bcgsc.ca>
2020-02-19 15:41:17 -0800

    Return variant rid

--- graphkb/constants.py
+++ graphkb/constants.py
@@ -1,11 +1,14 @@
-GENERIC_RETURN_PROPERTIES = [
-    'name',
+BASE_RETURN_PROPERTIES = [
     '@rid',
     '@class',
+]
+
+GENERIC_RETURN_PROPERTIES = [
+    'name',
     'sourceId',
     'sourceIdVersion',
     'source.name',
     'source.@rid',
     'displayName',
     'deprecated',
-]
+] + BASE_RETURN_PROPERTIES
