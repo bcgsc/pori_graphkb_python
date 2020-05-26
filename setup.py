c740a7f512d6e576a8b78f155479d46cddd65e02
Caralyn Reisle <creisle@bcgsc.ca>
2020-05-26 14:58:53 -0700

    Require typing extensions

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 # Dependencies required to use your package
-INSTALL_REQS = ['requests==2.22.0']
+INSTALL_REQS = ['requests==2.22.0', 'typing_extensions>=3.7.4.2']
 
 # Dependencies required only for running tests
 TEST_REQS = ['pytest', 'pytest-runner', 'pytest-cov']
