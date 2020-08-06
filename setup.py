d2886363c80c9a2ea2080420f72ec401ece59068
Caralyn Reisle <creisle@bcgsc.ca>
2020-08-06 15:54:57 -0700

    Bump version to 1.4.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ except Exception:
 
 setup(
     name='graphkb',
-    version='1.3.7',
+    version='1.4.0',
     description='python adapter for interacting with the GraphKB API',
     url='https://github.com/bcgsc/pori_graphkb_python',
     packages=find_packages(),
