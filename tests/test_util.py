d06ed917a1a8204944a89837c74394c4eaf42a51
mathieulemieux <mathieu@mathieulemieux.com>
2023-05-03 15:37:49 -0700

    black formating test_util

--- tests/test_util.py
+++ tests/test_util.py
@@ -2,7 +2,8 @@ import pytest
 import os
 from graphkb import util, GraphKBConnection
 
-class OntologyTerm():
+
+class OntologyTerm:
     def __init__(self, name, sourceId, displayName):
         self.name = name
         self.sourceId = sourceId
@@ -57,15 +58,16 @@ class TestOntologyTermRepr:
     @pytest.mark.parametrize(
         'termObjOpt,termRepr',
         [
-            [{"displayName": 'abc123', "name": '', "sourceId": ''}, 'abc123' ],
-            [{"displayName": '', "name": '', "sourceId": 'abc123'}, 'abc123' ],
-            [{"displayName": '', "name": 'abc123', "sourceId": ''}, 'abc123' ],
+            [{"displayName": 'abc123', "name": '', "sourceId": ''}, 'abc123'],
+            [{"displayName": '', "name": '', "sourceId": 'abc123'}, 'abc123'],
+            [{"displayName": '', "name": 'abc123', "sourceId": ''}, 'abc123'],
             [{"displayName": '', "name": '', "sourceId": ''}, ''],
         ],
     )
     def test_ontologyTermRepr_obj(self, termObjOpt, termRepr):
         termObj = OntologyTerm(**termObjOpt)
         assert util.ontologyTermRepr(termObj) == termRepr
+
 
 class TestStripParentheses:
     @pytest.mark.parametrize(
@@ -79,6 +81,7 @@ class TestStripParentheses:
     )
     def test_stripParentheses(self, breakRepr, StrippedBreakRepr):
         assert util.stripParentheses(breakRepr) == StrippedBreakRepr
+
 
 class TestStripRefSeq:
     @pytest.mark.parametrize(
@@ -101,9 +104,26 @@ class TestStripDisplayName:
             [{'displayName': 'ABL1:p.T315I', 'withRef': False, 'withRefSeq': True}, 'p.T315I'],
             [{'displayName': 'ABL1:p.T315I', 'withRef': True, 'withRefSeq': False}, 'ABL1:p.315I'],
             [{'displayName': 'ABL1:p.T315I', 'withRef': False, 'withRefSeq': False}, 'p.315I'],
-            [{'displayName': 'chr3:g.41266125C>T', 'withRef': False, 'withRefSeq': False}, 'g.41266125>T'],
-            [{'displayName': 'chrX:g.99662504_99662505insG', 'withRef': False, 'withRefSeq': False}, 'g.99662504_99662505insG'],
-            [{'displayName': 'chrX:g.99662504_99662505dup', 'withRef': False, 'withRefSeq': False}, 'g.99662504_99662505dup'],
+            [
+                {'displayName': 'chr3:g.41266125C>T', 'withRef': False, 'withRefSeq': False},
+                'g.41266125>T',
+            ],
+            [
+                {
+                    'displayName': 'chrX:g.99662504_99662505insG',
+                    'withRef': False,
+                    'withRefSeq': False,
+                },
+                'g.99662504_99662505insG',
+            ],
+            [
+                {
+                    'displayName': 'chrX:g.99662504_99662505dup',
+                    'withRef': False,
+                    'withRefSeq': False,
+                },
+                'g.99662504_99662505dup',
+            ],
             # TODO: [{'displayName': 'VHL:c.330_331delCAinsTT', 'withRef': False, 'withRefSeq': False}, 'c.330_331delinsTT'],
             # TODO: [{'displayName': 'VHL:c.464-2G>A', 'withRef': False, 'withRefSeq': False}, 'c.464-2>A'],
         ],
@@ -120,7 +140,11 @@ class TestStringifyVariant:
             ['VHL:c.345C>G', {'withRef': False, 'withRefSeq': True}, 'c.345C>G'],
             ['VHL:c.345C>G', {'withRef': True, 'withRefSeq': False}, 'VHL:c.345>G'],
             ['VHL:c.345C>G', {'withRef': False, 'withRefSeq': False}, 'c.345>G'],
-            ['(LMNA,NTRK1):fusion(e.10,e.12)', {'withRef': False, 'withRefSeq': False}, 'fusion(e.10,e.12)'],
+            [
+                '(LMNA,NTRK1):fusion(e.10,e.12)',
+                {'withRef': False, 'withRefSeq': False},
+                'fusion(e.10,e.12)',
+            ],
             ['ABCA12:p.N1671Ifs*4', {'withRef': False, 'withRefSeq': False}, 'p.1671Ifs*4'],
             ['x:y.p22.33copyloss', {'withRef': False, 'withRefSeq': False}, 'y.p22.33copyloss'],
             # TODO: ['MED12:p.(?34_?68)mut', {'withRef': False, 'withRefSeq': False}, 'p.(34_68)mut'],
@@ -141,7 +165,7 @@ class TestStringifyVariant:
             ['#158:35317', 1652734056311, 'c.1>G'],
         ],
     )
-    def test_stringifyVariant_positional(self, conn, rid,createdAt,stringifiedVariant):
+    def test_stringifyVariant_positional(self, conn, rid, createdAt, stringifiedVariant):
         opt = {'withRef': False, 'withRefSeq': False}
         variant = conn.get_record_by_id(rid)
         if variant and variant.get('createdAt', None) == createdAt:
